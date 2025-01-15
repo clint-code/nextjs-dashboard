559bd31260aa110e39ebba5c97552a835e27bb1a
Clinton Mutinda <cmutinda@mfs.co.ke>
2025-01-15 14:52:52 +0300

    Fetching data for the Card, RevenueChart and LatestInvoices components

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -16,7 +16,7 @@ export default async function LatestInvoices({
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         {/* NOTE: Uncomment this code in Chapter 7 */}
 
-        {/* <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -28,14 +28,16 @@ export default async function LatestInvoices({
                   },
                 )}
               >
+
                 <div className="flex items-center">
                   <Image
                     src={invoice.image_url}
                     alt={`${invoice.name}'s profile picture`}
                     className="mr-4 rounded-full"
-                    width={32}
-                    height={32}
+                    width={50}
+                    height={50}
                   />
+
                   <div className="min-w-0">
                     <p className="truncate text-sm font-semibold md:text-base">
                       {invoice.name}
@@ -44,16 +46,20 @@ export default async function LatestInvoices({
                       {invoice.email}
                     </p>
                   </div>
+
                 </div>
+
                 <p
                   className={`${lusitana.className} truncate text-sm font-medium md:text-base`}
                 >
                   {invoice.amount}
                 </p>
+
               </div>
             );
           })}
-        </div> */}
+        </div>
+
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
